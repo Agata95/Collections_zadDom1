5cfa283acb953d2b58f2b88b9cd51bbcd74da4dc
Agata <agata.weltrowska@outlook.com>
2019-06-06 13:01:31 +0200

    first commit

---
+++ src/main/java/com/javagda25/Collections_zadDom1/Main.java
@@ -0,0 +1,24 @@
+package com.javagda25.Collections_zadDom1;
+
+// aplikacja do rejestracji klientów na wizyty u dentysty/ortodonty, oraz do prowadzenia dokumentacji pacjentów.
+//
+// stwórz klasę Patient, która będzie przechowywała dane osobowe pacjenta
+// stwórz klasę RegistrationRequest, która będzie przechowywała szczegóły dotyczące danej wizyty, takie jak: pacjent,
+// data, imię i nazwisko lekarza, do którego rezerwowana jest wizyta, cena wizyty i dodatkowe szczegóły oraz
+// informacje o pacjencie (obiekt Patient)
+// stwórz klasę RegistrationDesk, która będzie miała metodę:
+// - register(RegistrationRequest request):long, zwracającą numer identyfikacyjny nowej wizyty
+// - getRequest(long requestId):RegistrationRequest, zwracającą dane wizyty
+// - getPatientInfo(long requestId):Patient, zwracającą dane osobowe pacjenta
+// -* getPatientVisits(String name, String surname):List<RegistrationRequest> - zwracającą listę (wszystkich)
+// wizyt podanego pacjenta.
+// -* getPatientVisits(Patient patient):List<RegistrationRequest> - zwracającą listę (wszystkich) wizyt podanego
+// pacjenta.
+//
+// Zadanie wykonaj na liście. Zadanie wymaga umiejętnosci przeszukiwania listy.
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
